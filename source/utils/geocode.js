923e26b869078b116414599d36fd89b1a876fd76
pramodksh <pramodkumarsh3@gmail.com>
2020-12-04 00:34:19 +0530

    configured geocode and weather files

--- source/utils/geocode.js
+++ source/utils/geocode.js
@@ -1,21 +1,25 @@
 const needle = require('needle')
-const key = require('../../key')
-// const { GeoAPIkey } = require('./key')
+const { GeoAPIkey } = require('../../key')
 
 const geoFunction = (address,callback)=>{
-    
-    const geocodeURL='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+'.json?access_token='+ GeoAPIkey +'&limit=1'
+    var key = process.env.GeoAPIkey || GeoAPIkey;
+    const geocodeURL='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+'.json?access_token='+ key +'&limit=1'
     
     needle.get(geocodeURL,{ json:true },(error,response,body)=>{
         const jsonData = JSON.parse(body);
         const {features}= jsonData;
+        const statusCode = response.statusCode;
         if(error){
+            callback("Check the access token you used in the query", undefined);
+        } else if (statusCode === 403){
+            callback("There may be an issue with your account. Check your Account page for more details.",undefined);
+        }else if(statusCode === 404){
+            callback("Check the endpoint you used in the query.",undefined)
+        } else if (statusCode === 401) {
             callback('Unable to find location. Try another search.',undefined);
-        }
-        else if (jsonData.features.length === 0) {
+        } else if (jsonData.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
-        }
-        else{
+        } else{
             const long = features[0].center[0];
             const lat = features[0].center[1];
             const location = features[0].place_name;
--- source/utils/weather.js
+++ source/utils/weather.js
@@ -1,9 +1,9 @@
 const needle = require('needle');
-const key = require('../../key');
-// const { weatherAPIkey } = require('./key');
+const { weatherAPIkey } = require('../../key');
 
 const weatherFunction = (long, lat, callback) => {
-    const URL = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid='+ weatherAPIkey +'&limit=1'
+    var key =  process.env.WeatherAPIkey || weatherAPIkey;
+    const URL = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid='+ key +'&limit=1'
     needle.get(URL, (error, response, body) => {
         if (error) {
             callback(error,undefined);
